eecb6d4ab8f340dd2071e6ab1aa7f0ad6e28d527
Sonu Kumar <thesonukumar357@gmail.com>
2024-06-02 16:36:39 +0530

    updated

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import './navbar.css';
+
+const Navbar = () => {
+  return (
+    <>
+    <nav className="navbar">
+      <div className="navbar-logo">
+        {/* <img src="lovely.png" alt="" /> */}
+        lovely bands
+      </div>
+      <div className="navbar-search">
+        <input type="text" placeholder="Search..." />
+        <button type="submit">
+          <img src="search-icon.webp" alt="Search" className="search-icon" />
+        </button>
+      </div>
+      <ul className="navbar-links">
+        <li><a href="#home">Home</a></li>
+        <li><a href="#products">Products</a></li>
+        <li><a href="#about">About</a></li>
+        <li><a href="#contact">Contact</a></li>
+      </ul>
+      <div className="navbar-user">
+        <a href="#login">Login</a>
+        <a href="#register">Register</a>
+      </div>
+      <div className="navbar-cart">
+        <a href="#cart">
+          <img src="cart-icon.png" alt="Cart" className="cart-icon" />
+          <span className="cart-count">0</span>
+        </a>
+      </div>
+      
+    </nav>
+    <div >
+      <p style={{textAlign:'center'}}>  🎉Special 10% discount for new users    🎉</p>
+    </div>
+    </>
+  );
+}
+
+export default Navbar;
