a4624d33280540350402ebf671be416c805ab58c
Sonu Kumar <thesonukumar357@gmail.com>
2024-06-04 17:12:15 +0530

    added custom

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,23 @@
 import Navbar from "./components/Navbar/Navbar"
-import Home from "./pages/Home"
+import Home from "./pages/Home/Home"
 import './App.css'
+import Lanyard from "./pages/lanyard/Lanyard"
+import Custom from "./pages/custom/Custom"
+import Footer from "./components/Footer/Footer"
+import { BrowserRouter, Routes,Route } from "react-router-dom"
+
 
 function App() {
   return (
     <>
-      <Navbar />
-      <Home />
+    <Navbar/>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/custom-lanyard" element={<Custom />} />
+        </Routes>
+      </BrowserRouter>
+      <Footer/>
     </>
   )
 }
